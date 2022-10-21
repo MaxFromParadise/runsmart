9ebdc80f060df556e67e22b01fcff19a824000ad
Maxim <maxdemented372@gmail.com>
2022-10-21 17:17:00 +0600

    carousel

---
+++ src/js/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        autoplay: true,
+        autoplaySpeed: 3000,
+        prevArrow: '<button type="button" class="slick-prev"><img src="../img/prev.png"></img></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../img/next.png"></img></button>',
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    dots: true,
+                    arrows: false
+                }
+            }
+        ]
+    });
+  });
