b76266a13e06223cff734b585f8bc47b2f32db52
Maxim <maxdemented372@gmail.com>
2022-10-21 17:22:34 +0600

    carouselNewPath

--- src/js/script.js
+++ src/js/script.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
         autoplay: true,
         autoplaySpeed: 3000,
         prevArrow: '<button type="button" class="slick-prev"><img src="img/prev.png"></img></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="../img/next.png"></img></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="img/next.png"></img></button>',
         responsive: [
             {
                 breakpoint: 992,
