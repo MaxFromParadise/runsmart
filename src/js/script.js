abafc05777bedc7af75c074c226316a5282891cc
MaxFromParadise <112644100+MaxFromParadise@users.noreply.github.com>
2022-10-21 17:20:28 +0600

    newPathOfArrows

--- src/js/script.js
+++ src/js/script.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     $('.carousel__inner').slick({
         autoplay: true,
         autoplaySpeed: 3000,
-        prevArrow: '<button type="button" class="slick-prev"><img src="../img/prev.png"></img></button>',
+        prevArrow: '<button type="button" class="slick-prev"><img src="img/prev.png"></img></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="../img/next.png"></img></button>',
         responsive: [
             {
